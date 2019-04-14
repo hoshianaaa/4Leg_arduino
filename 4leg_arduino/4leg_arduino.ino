32a8e7abe97cd43afefa040de16b81ff3985071e
Ryotaro Hoshina <hoshina12345@gmail.com>
2019-04-14 11:46:28 +0900

    read gryo degree

--- 4leg_arduino/4leg_arduino.ino
+++ 4leg_arduino/4leg_arduino.ino
@@ -2,6 +2,9 @@
 #include <Servo.h>
 #include "gyro.h"
 
+#define DT 15
+
+#define DEBUG 1
 
 Servo servo[12];
 const int LED_R = 20;
@@ -9,11 +12,10 @@ const int LED_L = A12;
 const int SW_R = 30;//右スイッチ
 const int SW_L = 44;//左スイッチ
 
-double data[3];
-
 void flash() {
   static int flag;
   static int degree;
+  double gyro[3];
 
   if (flag == false) {
     for (int i = 0; i < 12; i++)servo[i].write(degree);
@@ -25,8 +27,10 @@ void flash() {
     for (int i = 0; i < 12; i++)servo[i].write(degree);
     degree--;
     if (degree == 0)flag = false;
-
   }
+
+  GyroRead(DT);
+  Serial.println(GyroDeg[0]);
 }
 
 
@@ -40,10 +44,10 @@ void setup() {
   pinMode(SW_R, INPUT_PULLUP);
   pinMode(SW_L, INPUT_PULLUP);
 
-  FlexiTimer2::set(15, 1.0 / 1000, flash); // 15 * 1ms ごとにタイマー割込み
+  FlexiTimer2::set(DT, 1.0 / 1000, flash); // DT * 1ms ごとにタイマー割込み
   FlexiTimer2::start();
 
-  gyro_init();
+  GyroInit();
 }
 
 
@@ -52,12 +56,6 @@ void loop() {
   if (digitalRead(SW_R) == HIGH)digitalWrite(LED_R, HIGH);
   else digitalWrite(LED_R, LOW);
 
-  gyro_read(data);
 
-  for(int i=0;i<3;i++){
-    Serial.print(data[i]);
-    Serial.print(" ");
-  }
-  Serial.println("");
-  
+
 }
--- 4leg_arduino/gyro.h
+++ 4leg_arduino/gyro.h
@@ -4,22 +4,31 @@
 #include <Wire.h>
 #include <Adafruit_L3GD20.h>
 
-const int GYRO_CS = 49;
-const int GYRO_DO = 50;
-const int GYRO_DI = 51;
-const int GYRO_CLK = 52;
+const int GYRO_CS = 49;  // mega CS
+const int GYRO_DO = 50;  // mega MISO
+const int GYRO_DI = 51;  // mega MOSI
+const int GYRO_CLK = 52; // mega SCK
+
+const double DRIFT[3] = { -0.0163681, 4.55715, -0.6529};
+const double DRIFT_2[3] = { -0.016952, 0.373748, -0.057698};
+const double DRIFT_3[3] = { -0.00729, 0.01722, 0.02919};
+
+double GyroDeg[3];
+
 
 Adafruit_L3GD20 gyro(GYRO_CS, GYRO_DO, GYRO_DI, GYRO_CLK);
 
-void gyro_init() {
+void GyroInit() {
   gyro.begin(gyro.L3DS20_RANGE_250DPS);
 }
 
-gyro_read(double* data) {
+void GyroRead(int DeltaTime) {
+  
   gyro.read();
-  data[0] = gyro.data.x;
-  data[1] = gyro.data.y;
-  data[2] = gyro.data.z;
+  GyroDeg[0] += (gyro.data.x - DRIFT[0] - DRIFT_2[0] - DRIFT_3[0]) * DeltaTime / 1000;
+  GyroDeg[1] += (gyro.data.y - DRIFT[1] - DRIFT_2[1] - DRIFT_3[1]) * DeltaTime / 1000;
+  GyroDeg[2] += (gyro.data.z - DRIFT[2] - DRIFT_2[2] - DRIFT_3[2]) * DeltaTime / 1000;
+
 }
 
 #endif
